b8f3f5ddc5e221fe55648a54c250b2366e787be5
SyntheticAutomation <mclampet@msudenver.edu>
2018-12-15 10:56:53 -0700

    Completes item migration

--- db/migrate/20181215175126_create_items.rb
+++ db/migrate/20181215175126_create_items.rb
@@ -2,7 +2,11 @@ class CreateItems < ActiveRecord::Migration[5.1]
   def change
     create_table :items do |t|
       t.string :name
-      t.
+      t.string :description
+      t.string :image_url
+      t.integer :inventory_qty
+      t.integer :price
+      t.timestamps
     end
   end
 end
