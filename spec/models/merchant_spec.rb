5ce3b61250a78067d503dc4766510dc98b778477
Anten Skrabec <cebarks@gmail.com>
2018-12-16 11:32:51 -0700

    adds feature and model level testing for merchant index page

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,12 +1,9 @@
 require 'rails_helper'
 
-describe User, type: :model do
+describe Merchant, type: :model do
   describe 'Validations' do
     it {should validate_presence_of(:name)}
-    it {should validate_presence_of(:address)}
     it {should validate_presence_of(:city)}
     it {should validate_presence_of(:state)}
-    it {should validate_presence_of(:zipcode)}
-    it {should validate_presence_of(:email)}
   end
 end
