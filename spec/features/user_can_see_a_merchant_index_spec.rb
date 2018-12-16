8bfd72e1958729e694a8ecf45c01f09c115a7004
Anten Skrabec <cebarks@gmail.com>
2018-12-16 15:19:28 -0700

    implements merchant index page shows only active merchants

--- spec/features/user_can_see_a_merchant_index_spec.rb
+++ spec/features/user_can_see_a_merchant_index_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 RSpec.describe "As a visitor" do
   describe "When i visit the marchant's index page at \"/merchants\"" do
     before(:each) do
-      # FIXME: Merchants needs to have one that is active and one that is not
-      @merchant_1 = Merchant.new(name: "test1", city: "Denver", state: "Colorado")
-      @merchant_2 = Merchant.new(name: "test2", city: "Moab", state: "Utah")
+      @merchant_1 = Merchant.create!(name: "test1", city: "Denver", state: "Colorado", status: true)
+      @merchant_2 = Merchant.create!(name: "test2", city: "Moab", state: "Utah", status: false)
 
       visit merchants_path
     end
-
+    
     it "I see all merchants in the system who are active" do
       expect(page).to have_content(@merchant_1.name)
       expect(page).to_not have_content(@merchant_2.name)
