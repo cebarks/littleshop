3f6092232932e1bb05f84cb8be16d85f83da1f8e
Anten Skrabec <cebarks@gmail.com>
2018-12-16 15:18:41 -0700

    adds status column to merchants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181215181804) do
+ActiveRecord::Schema.define(version: 20181216220439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20181215181804) do
     t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "status", default: true
   end
 
   create_table "order_items", force: :cascade do |t|
