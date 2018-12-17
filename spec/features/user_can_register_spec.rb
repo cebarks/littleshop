5b9f7082a660bb8d2ce34c7ff5d8e033a7844bd5
Anten Skrabec <cebarks@gmail.com>
2018-12-17 15:05:07 -0700

    base stucture for feature test

---
+++ spec/features/user_can_register_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor" do
+  describe "When I click the 'register' link in the nav bar" do
+    it "I am on the user registration page" do
+      
+    end
+
+    it "I can fill out a form to create a new user" do
+
+    end
+
+    it "requires a unqiue email to sign up" do
+
+    end
+  end
+end
