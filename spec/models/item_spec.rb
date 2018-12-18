e03070ead351fed91c350de0b90684cf23baa367
Anten Skrabec <cebarks@gmail.com>
2018-12-18 13:35:00 -0700

    build out Item model and spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   describe 'Validations' do
     it {should belong_to(:user)}
+    it {should have_many(:order_items)}
+    it {should have_many(:orders).through(:order_items)}
   end
 end
