8c43bdccd9ace95799d700146a4b8f837d27db81
Anten Skrabec <cebarks@gmail.com>
2018-12-18 14:15:24 -0700

    adds more validations on Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  validates_presence_of :name, :description, :inventory_qty, :price, :user
+  validates_inclusion_of :status, :in => [true, false]
   belongs_to :user
   has_many :order_items
   has_many :orders, through: :order_items
