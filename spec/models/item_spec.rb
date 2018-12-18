9ee2382260838302f89128e194063162a53cecfc
Mary Goodhart <marya.grace5@gmail.com>
2018-12-18 15:36:56 -0700

    user story 3 missing two pieces

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,10 +13,7 @@ RSpec.describe Item, type: :model do
   end
   describe 'instance methods' do
     describe '.average_fulfillment_time' do
-      it 'should return average fulfillment time for specific item' do
-        merchant_1 = create(:user, role: 1)
-        item_1 = create(:item, user: merchant_1)
-        binding.pry
+      xit 'should return average fulfillment time for specific item' do
 
       end
     end
