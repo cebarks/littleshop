e43389e58cf2ad79b3b23e7250413d0f2eb5f3cf
Anten Skrabec <cebarks@gmail.com>
2018-12-18 00:19:39 -0700

    majority of user registration implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,35 @@
 class UsersController < ApplicationController
-
- 
   def new
+    @user = User.new
   end
-  
+
+  def create
+    @user = User.new(user_params)
+
+    render :new if user_params[:confirm_password]
+
+    if @user.save
+      redirect_to profile_path(@user)
+    else
+      render :new
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    parms = params.require(:user).permit(:email, :password, :confirm_password, :name, :address, :city, :state, :zipcode)
+    if parms[:password] == parms[:confirm_password]
+      parms.delete(:confirm_password)
+    end
+    parms
   end
 end
