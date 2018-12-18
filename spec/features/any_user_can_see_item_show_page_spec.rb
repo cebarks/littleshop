5fa2bcfe55474446fec928e7ba28a0a62189656e
Mary Goodhart <marya.grace5@gmail.com>
2018-12-17 18:10:24 -0700

    adds code to show page and show method in items controller

--- spec/features/any_user_can_see_item_show_page_spec.rb
+++ spec/features/any_user_can_see_item_show_page_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 describe 'as any user in the system' do
   describe 'when i visit an items show page from the items catalogue' do
     #need to add image!
-    xit 'should see all the information for that particular item except for image' do
+    it 'should see all the information for that particular item except for image' do
       merchant_1 = create(:user, role: 1)
-      item = create(:item, user_id: merchant_1.id)
+      item = create(:item, user: merchant_1)
 
       visit item_path(item)
 
       expect(page).to have_content(item.name)
       expect(page).to have_content(item.description)
       expect(page).to have_content(item.inventory_qty)
-      expect(page).to have_content(merchant_1.name)
+      expect(page).to have_content(item.user.name)
       expect(page).to have_content(item.price)
-      expect(page).to have_content(item.email)
+      save_and_open_page
     end
   end
 end
