b768ba08bb71ad2798103fd9fb8539a502cd1e61
Mary Goodhart <marya.grace5@gmail.com>
2018-12-17 18:55:51 -0700

    adds tests for last two items in user story 16, not passing, currently skipped

--- spec/features/any_user_can_see_item_show_page_spec.rb
+++ spec/features/any_user_can_see_item_show_page_spec.rb
@@ -14,7 +14,34 @@ describe 'as any user in the system' do
       expect(page).to have_content("Quantity Remaining: #{item.inventory_qty}")
       expect(page).to have_content("Merchant: #{item.user.name}")
       expect(page).to have_content("Price: $#{item.price}")
+    end
+    xit 'should see the average amount of time it takes a user-merchant to fulfill' do
+      merchant_1 = create(:user, role: 1)
+      item = create(:item, user: merchant_1)
 
+      visit item_path(item)
+
+      expect(page).to have_content("Average Time to Fulfill: #{merchant_1.average_fulfillment_time}")
+    end
+  end
+end
+describe 'as a visitor or regular user' do
+  describe 'when i visit an items show page from the items catalogue' do
+    xit 'should also see a link to add this item to the cart' do
+      merchant_1 = create(:user, role: 1)
+      user_1 = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+      item = create(:item, user: merchant_1)
+
+      visit item_path(item)
+
+      expect(page).to have_content(item.name)
+      expect(page).to have_content("Description: #{item.description}")
+      expect(page).to have_content("Quantity Remaining: #{item.inventory_qty}")
+      expect(page).to have_content("Merchant: #{item.user.name}")
+      expect(page).to have_content("Price: $#{item.price}")
     end
   end
 end
