e17ecba2501c2a2c0692ac0cdf60bbed555c1ee6
SyntheticAutomation <mclampet@msudenver.edu>
2018-12-18 19:15:23 -0700

    Adds model test for story_15

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe Item, type: :model do
     it {should validate_presence_of(:price)}
     it {should validate_presence_of(:user)}
   end
+  describe 'Class Methods' do
+    it '.amount_sold' do
+      item_1 = Item.create(name: "Amaretto Sour", description: "a sweet nutty cocktail that calls for a full bodied spirit to pair with the bright citrus and sugar. di Amore Amaretto is an excellent choice with its rich body and caramel, almond flavors.", image_url: "https://bit.ly/2BoIMFi", inventory_qty: rand(1..9999), price: Faker::Number.decimal(2))
+      order_1 = Order.create(status: "complete")
+      oi_1 = OrderItem.create(item_id: item_1, order_id: order_1, quantity: 49, price: item_1.price)
+      require "pry"; binding.pry
+      expect(Item.amount_sold(1)).to eq(OrderItem.last.quantity)
+    end
+  end
 end
