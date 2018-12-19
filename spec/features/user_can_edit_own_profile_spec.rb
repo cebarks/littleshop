ccdcd6b84da857662df532200add0817c402c73a
Mary Goodhart <marya.grace5@gmail.com>
2018-12-18 17:46:32 -0700

    finishes test for edit profile link

--- spec/features/user_can_edit_own_profile_spec.rb
+++ spec/features/user_can_edit_own_profile_spec.rb
@@ -5,9 +5,13 @@ describe 'as a registered user' do
     it 'should see a link to edit profile date' do
       registered_user = create(:user)
 
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(registered_user)
 
+      expect(current_path).to eq(user_path(registered_user))
 
+      click_link("Edit Profile")
 
+      expect(current_path).to eq('/profile/edit')
     end
   end
 end
