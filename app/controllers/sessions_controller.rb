cae583869a09d10cb93bee9c3dc08db56e3c222c
Mary Goodhart <marya.grace5@gmail.com>
2018-12-18 17:15:08 -0700

    began login
    started login tests and implementing code for user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user_id
+      redirect_to profile_path
+    else
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/register', to: "users#new"
   get '/login', to: "sessions#new"
   get '/profile', to: "users#profile", as: "profile"
+  post '/login', to: 'sessions#create'
 
   resources :items
   resources :users, only:[:show, :edit, :update, :create]
