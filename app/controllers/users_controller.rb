643898101d489abf3598295898dce11a7e31f31f
Mary Goodhart <marya.grace5@gmail.com>
2018-12-19 08:08:59 -0700

    moving forward with testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,9 +43,13 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to profile_path
+    else
+      render :edit
+    end
     #an if statement to update something if it doesnt match what is currently
     # in the system
-
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,10 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new"
   get '/profile', to: "users#profile", as: "profile"
   get '/profile/:id/edit', to: "users#edit", as: "edit_profile"
+  post '/profile', to: "users#update"
 
   resources :items
-  resources :users, only:[:show, :edit, :update, :create]
+  resources :users, only:[:show, :create, :update]
   resources :merchants
   resources :orders
   resources :cart, only: [:index]
