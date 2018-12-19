a3e79a323744f2568687ff620a530242b9f7518b
Mary Goodhart <marya.grace5@gmail.com>
2018-12-18 17:38:53 -0700

    begins testing for user editing profile page

---
+++ spec/features/user_can_edit_own_profile_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'as a registered user' do
+  describe 'when i visit my profile page' do
+    it 'should see a link to edit profile date' do
+      registered_user = create(:user)
+
+      
+
+
+    end
+  end
+end
---
+++ spec/features/user_can_login_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor" do
+  describe "when i visit the login path" do
+    it "sees a field to enter my email and password" do
+      email = "email1@gmail.com"
+      password = "123"
+
+      visit login_path
+
+      fill_in :user_email, with: email
+      fill_in :user_password, with: password
+
+      click_on "Login"
+
+      expect(current_path).to eq(profile_path)
+    end
+  end
+end
