c39bf441947ddb0d5f8ac2bd16713554f5178c91
Mary Goodhart <marya.grace5@gmail.com>
2018-12-22 13:19:47 -0700

    refactors one test
    all tests passing

--- spec/features/admin_can_see_default_user_index_page_spec.rb
+++ spec/features/admin_can_see_default_user_index_page_spec.rb
@@ -5,7 +5,7 @@ describe 'As an admin user' do
     it 'should be directed to the admin_user_index page' do
       admin = create(:admin)
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      post_login(admin)
 
       visit root_path
 
