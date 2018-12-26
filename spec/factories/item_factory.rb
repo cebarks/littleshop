cda94f74268ee1ea5aefabf73d5b46071f67f2bd
Anten Skrabec <cebarks@gmail.com>
2018-12-26 14:44:18 -0700

    updated item and order factories, created OrderItem factory

--- spec/factories/item_factory.rb
+++ spec/factories/item_factory.rb
@@ -6,6 +6,7 @@ FactoryBot.define do
     description {Faker::Coffee.notes}
     inventory_qty { 9 }
     price { 10 }
+    image_url { "https://visualpharm.com/assets/783/Facebook%20Like-595b40b65ba036ed117d1f0f.svg" }
 
     association :user, factory: :merchant
 
