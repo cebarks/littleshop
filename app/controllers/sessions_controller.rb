1a478ae06e98ad230584b0d731d7191c0119bde6
Mary Goodhart <marya.grace5@gmail.com>
2018-12-28 11:01:20 -0500

    adds user && to authenicate method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,17 +8,17 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:user_email])
-    
-    if user.authenticate(params[:user_password])
+
+    if user && user.authenticate(params[:user_password])
       unless user.enabled?
         flash[:notice] = "Your account has been disabled by an administrator."
         render :new
         return
       end
-      
+
       session[:user_id] = user.id
       flash[:notice] = "You are now logged in!"
-      
+
       login_redirect(user.role)
     else
       flash[:notice] = "Your credentials were incorrect!"
