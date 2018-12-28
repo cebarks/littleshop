e42a14122b472cb85b32738342650cc9f308fd22
Mary Goodhart <marya.grace5@gmail.com>
2018-12-28 08:00:40 -0500

    adds test for incrementing in cart

--- spec/features/user_can_manipulate_cart_qty_spec.rb
+++ spec/features/user_can_manipulate_cart_qty_spec.rb
@@ -1,38 +1,52 @@
 require 'rails_helper'
 
-describe 'As a visitor or registered user' do
-  describe 'When I have items in and visit my cart' do
-    it 'should I see a separate button to remove each item from my cart' do
-      merchant = create(:user, role: 1)
-      user = create(:user)
-      item_1 = create(:item, user: merchant)
-      item_2 = create(:item, user: merchant)
-      order_1 = Order.create!(status: 3, created_at: 2.days.ago, user: user)
-      order_2 = Order.create!(status: 3, created_at: 3.days.ago, user: user)
-      OrderItem.create!(item: item_1, order: order_1, quantity: 1, price: 1)
-      OrderItem.create!(item: item_2, order: order_2, quantity: 1, price: 1)
-      visit item_path(item_1)
-      click_button('Add To Cart')
+describe 'As a visitor or registered user When I have items in and visit my cart' do
+  describe 'should I see a separate button to remove each item from my cart' do
+    describe 'I see a button or link to increment the count of items I want to purchase' do
+      before(:each) do
+        @merchant = create(:user, role: 1)
+        @user = create(:user)
+        @item_1 = create(:item, user: @merchant)
+        @item_2 = create(:item, user: @merchant)
+        order_1 = Order.create!(status: 3, created_at: 2.days.ago, user: @user)
+        order_2 = Order.create!(status: 3, created_at: 3.days.ago, user: @user)
+        OrderItem.create!(item: @item_1, order: order_1, quantity: 1, price: 1)
+        OrderItem.create!(item: @item_2, order: order_2, quantity: 1, price: 1)
+        visit item_path(@item_1)
+        click_button('Add To Cart')
 
-      visit item_path(item_2)
-      click_button('Add To Cart')
+        visit item_path(@item_2)
+        click_button('Add To Cart')
 
-      visit cart_path
-
-      within("#item-#{item_1.name}") do
-        expect(page).to have_selector(:link_or_button, 'Remove Item')
+        visit cart_path
       end
+      it 'clicking this button will remove the item but not other items' do
 
-      within("#item-#{item_2.name}") do
-        expect(page).to have_selector(:link_or_button, 'Remove Item')
+        within("#item-#{@item_1.name}") do
+          expect(page).to have_selector(:link_or_button, 'Remove Item')
+        end
+
+        within("#item-#{@item_2.name}") do
+          expect(page).to have_selector(:link_or_button, 'Remove Item')
+        end
+
+        within("#item-#{@item_1.name}") do
+          click_link('Remove Item')
+        end
+        expect(current_path).to eq(cart_path)
+
+        expect(page).to_not have_content("#{@item_1.name}")
+
       end
+      it 'cannot increment the count beyond the merchants inventory size' do
+        within("#item-#{@item_1.name}") do
+          expect(page).to have_selector(:link_or_button, 'Add Another')
+        end
 
-      within("#item-#{item_1.name}") do
-        click_link('Remove Item')
+        within("#item-#{@item_2.name}") do
+          expect(page).to have_selector(:link_or_button, 'Add Another')
+        end
       end
-      expect(current_path).to eq(cart_path)
-
-      expect(page).to_not have_content("#{item_1.name}")
     end
   end
 end
@@ -40,10 +54,6 @@ end
 
 
 
-# - clicking this button will remove the item but not other items
-#
-# I see a button or link to increment the count of items I want to purchase
-# - I cannot increment the count beyond the merchant's inventory size
-#
+
 # I see a button or link to decrement the count of items I want to purchase
-# - If I decrement the count to 0 the item is immediately removed from my cart
+# If I decrement the count to 0 the item is immediately removed from my cart
