9819f51d6c5efb7bfddf0d56dbd28405dd4befe4
Mary Goodhart <marya.grace5@gmail.com>
2018-12-28 14:08:00 -0500

    changes routes and tests to match user stories

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     # resources :users, only: [:show, :update], as: "merchants"
-    resources :users, only: [:index, :show], as: "users"
+    resources :users, only: [:index, :show, :update], as: "users"
     resources :merchants, only: [:index, :show, :update]
   end
 
