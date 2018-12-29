fdae98eb1c798a7d51fdacbeac4724bce139dda4
Anten Skrabec <cebarks@gmail.com>
2018-12-29 15:37:16 -0700

    adds styling to the login page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,6 +15,7 @@ Rails.application.config.assets.precompile += %w( user_show.css )
 Rails.application.config.assets.precompile += %w( order_show.css )
 Rails.application.config.assets.precompile += %w( cart_show.css )
 Rails.application.config.assets.precompile += %w( item_show.css )
+Rails.application.config.assets.precompile += %w( login.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
