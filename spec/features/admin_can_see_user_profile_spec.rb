e94c7e48a85ef7002f7e24e140f98ff6c651c2fb
SyntheticAutomation <mclampet@msudenver.edu>
2018-12-30 11:04:25 -0700

    Adds code for card 49

--- spec/features/admin_can_see_user_profile_spec.rb
+++ spec/features/admin_can_see_user_profile_spec.rb
@@ -1,14 +1,16 @@
-
 require 'rails_helper'
-
 
   describe 'As an admin' do
     context 'visiting a user profile page' do
       it 'sees all the information a user would see' do
 
         admin_1 = create(:admin)
-        user_1 = create(:user, role: 0)
-          allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_1)
+        merchant_1 = create(:merchant)
+        user_1 = create(:user)
+        order_1 = create(:order, user: user_1)
+        item_1 = create(:item, user: merchant_1)
+        OrderItem.create(item: item_1, order: order_1, quantity: 5, price: 100)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_1)
 
         visit admin_user_path(user_1)
 
@@ -20,7 +22,8 @@ require 'rails_helper'
         expect(page).to have_content(user_1.email)
         expect(page).not_to have_content(user_1.password)
         expect(page).to have_content(user_1.role)
-
+        expect(page).to have_css(".orders-table")
+        
         expect(page).to_not have_link("View My Items", href: dashboard_items_path)
       end
     end
