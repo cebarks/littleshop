1a950730081df53ae60fae8521c3962d815bfc8d
Mary Goodhart <mary.loves.pry@gmail.com>
2018-12-31 14:32:33 -0500

    takes out comments

--- spec/features/user_can_cancel_order_spec.rb
+++ spec/features/user_can_cancel_order_spec.rb
@@ -48,13 +48,3 @@ describe 'As a registered user when a single orders show page that is status:pen
     end
   end
 end
-
-
-
-# When I click the cancel button for an order, the following happens:
-# - Each row in the "order items" table is given a status of "unfulfilled"
-# - The order itself is given a status of "cancelled"
-# - Any item quantities in the order that were previously fulfilled have their quantities returned to their respective merchant's inventory for that item.
-# - I am returned to my profile page
-# - I see a flash message telling me the order is now cancelled
-# - And I see that this order now has an updated status of "cancelled"
