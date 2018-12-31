08a659ec0401b61a2eeca158ef5a09d90e69b260
Anten Skrabec <cebarks@gmail.com>
2018-12-30 20:33:42 -0700

    adds :pending trait to order factory

--- spec/factories/order_factory.rb
+++ spec/factories/order_factory.rb
@@ -7,6 +7,10 @@ FactoryBot.define do
       items_count { 2 }
     end
 
+    trait :pending do
+      status { "pending" }
+    end
+
     after(:create) do |order, evaluator|
       create_list(:order_item, evaluator.items_count, order: order)
     end
