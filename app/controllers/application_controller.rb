27a5c9ed2ca0b00b13ce87e836c3862deb7c6096
Mary Goodhart <mary.loves.pry@gmail.com>
2019-01-01 16:05:37 -0700

    cleans up small pieces of code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,4 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def not_found
-    render 'welcome/index', status: 404
-  end
 end
