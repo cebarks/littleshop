a72a4893ac32ef60e39a02a9b36ab18018937aa9
SyntheticAutomation <mclampet@msudenver.edu>
2019-01-01 19:02:36 -0700

    Adds final changes after going through stories

--- spec/features/visitor_must_login_register_to_checkout_spec.rb
+++ spec/features/visitor_must_login_register_to_checkout_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe "As a visitor" do
   describe "When I have items in my cart and I visit the cart show page" do
     it "I am told that I must register or login to finish the checkout" do
       visit cart_path
-
       expect(page).to have_content('You must register or login to finish the checkout process.')
 
       expect(page).to have_link('register', href: register_path)
